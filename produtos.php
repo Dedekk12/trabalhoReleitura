48cffd45c862e56915d44bc7a448251c7e868725
𝔄𝔫𝔡𝔯_ℭ@𝔯𝔩𝔬𝔰 <andrecarlosfreire@gmail.com>
2025-12-12 11:49:26 -0300

    Add files via upload

---
+++ produtos.php
@@ -0,0 +1,347 @@
+<?php require __DIR__ . "/header.php"; ?>
+
+<main class="produtos">
+
+
+    <div class="container">
+
+    <nav class="mt-4" aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Produtos</li>
+                
+            </ol>
+    </nav>
+
+
+    <div class="texto fs-3">
+
+        <h3 class="fw-bolder fs-1">Produtos Mais vendidos : </h3>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt deleniti ipsum reprehenderit suscipit illo! Perspiciatis sequi quod blanditiis neque, sit beatae atque a! Laboriosam suscipit placeat excepturi consequuntur nemo ipsam.</p>
+
+
+    </div>
+    <!--- texto --->
+
+
+    <main class="produtos">
+
+        <div class="container">
+
+            <div class="row">
+
+            <!-- Produto -->
+            <div class="col-md-3">
+                <div class="card card-produto rounded-0 border-0 ">
+                <a href="produto.php"><img src="assets/images.2/produtos/thumb/cartera-etc.jpg" class="card-img-top rounded-0" alt="..."></a>
+                <div class="card-body">
+
+                    <h5 class="card-title">Camiseta manga comprida</h5>
+
+                    <p class="card-text">
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+
+                    </p>
+
+                    <p class="card-text">R$ 59,90</p>
+
+                </div>
+                </div>
+            </div>
+
+            <!-- Produto -->
+            <div class="col-md-3">
+                <div class="card card-produto rounded-0 border-0 ">
+                <a href="produto.php"><img src="assets/images.2/produtos/thumb/camisas-etc.jpg" class="card-img-top rounded-0" alt="..."></a>
+                <div class="card-body">
+
+                    <h5 class="card-title">Camiseta manga comprida</h5>
+
+                    <p class="card-text">
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+
+                    </p>
+
+                    <p class="card-text">R$ 59,90</p>
+
+                </div>
+                </div>
+            </div>
+            <!-- /Produto -->
+
+            <!-- Produto -->
+            <div class="col-md-3">
+                <div class="card card-produto rounded-0 border-0">
+                <a href="produto.php"><img src="assets/images.2/produtos/thumb/camiseta-etc.jpg" class="card-img-top rounded-0" alt="..."></a>
+                <div class="card-body">
+
+                    <h5 class="card-title">Roupas Laranjas </h5>
+
+                    <p class="card-text">
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    </p>
+
+                    <p class="card-text">R$ 39,90</p>
+
+                </div>
+                </div>
+            </div>
+            <!-- /Produto -->
+
+            <!-- Produto -->
+            <div class="col-md-3">
+                <div class="card card-produto rounded-0 border-0">
+                <a href="produto.php"><img src="assets/images.2/produtos/thumb/chapeu-etc.jpg" class="card-img-top rounded-0" alt="..."></a>
+                <div class="card-body">
+
+                    <h5 class="card-title">Chápeus e Acessórios</h5>
+
+                    <p class="card-text">
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    </p>
+
+                    <p class="card-text">R$ 79,90</p>
+
+                </div>
+                </div>
+            </div>
+            <!-- /Produto -->
+
+            </div>
+
+        </div>
+        </main>
+        <!-- /.produtos -->
+
+
+
+
+
+        </div>
+        <!--- container --->
+
+    </main>
+
+    <main class="produtos">
+
+        <div class="container">
+
+            <div class="row">
+
+            <!-- Produto -->
+            <div class="col-md-3">
+                <div class="card card-produto rounded-0 border-0 ">
+                <a href="produto.php"><img src="assets/images.2/produtos/thumb/cartera-etc.jpg" class="card-img-top rounded-0" alt="..."></a>
+                <div class="card-body">
+
+                    <h5 class="card-title">Camiseta manga comprida</h5>
+
+                    <p class="card-text">
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+
+                    </p>
+
+                    <p class="card-text">R$ 59,90</p>
+
+                </div>
+                </div>
+            </div>
+
+            <!-- Produto -->
+            <div class="col-md-3">
+                <div class="card card-produto rounded-0 border-0 ">
+                <a href="produto.php"><img src="assets/images.2/produtos/thumb/camisas-etc.jpg" class="card-img-top rounded-0" alt="..."></a>
+                <div class="card-body">
+
+                    <h5 class="card-title">Camiseta manga comprida</h5>
+
+                    <p class="card-text">
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+
+                    </p>
+
+                    <p class="card-text">R$ 59,90</p>
+
+                </div>
+                </div>
+            </div>
+            <!-- /Produto -->
+
+            <!-- Produto -->
+            <div class="col-md-3">
+                <div class="card card-produto rounded-0 border-0">
+                <a href="produto.php"><img src="assets/images.2/produtos/thumb/camiseta-etc.jpg" class="card-img-top rounded-0" alt="..."></a>
+                <div class="card-body">
+
+                    <h5 class="card-title">Roupas Laranjas </h5>
+
+                    <p class="card-text">
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    </p>
+
+                    <p class="card-text">R$ 39,90</p>
+
+                </div>
+                </div>
+            </div>
+            <!-- /Produto -->
+
+            <!-- Produto -->
+            <div class="col-md-3">
+                <div class="card card-produto rounded-0 border-0">
+                <a href="produto.php"><img src="assets/images.2/produtos/thumb/chapeu-etc.jpg" class="card-img-top rounded-0" alt="..."></a>
+                <div class="card-body">
+
+                    <h5 class="card-title">Chápeus e Acessórios</h5>
+
+                    <p class="card-text">
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    </p>
+
+                    <p class="card-text">R$ 79,90</p>
+
+                </div>
+                </div>
+            </div>
+            <!-- /Produto -->
+
+            </div>
+
+        </div>
+        </main>
+        <!-- /.produtos -->
+
+
+
+
+
+        </div>
+        <!--- container --->
+
+    </main>
+
+    <main class="produtos mb-3">
+
+        <div class="container">
+
+            <div class="row">
+
+            <!-- Produto -->
+            <div class="col-md-3">
+                <div class="card card-produto rounded-0 border-0 ">
+                <a href="produto.php"><img src="assets/images.2/produtos/thumb/cartera-etc.jpg" class="card-img-top rounded-0" alt="..."></a>
+                <div class="card-body">
+
+                    <h5 class="card-title">Camiseta manga comprida</h5>
+
+                    <p class="card-text">
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+
+                    </p>
+
+                    <p class="card-text">R$ 59,90</p>
+
+                </div>
+                </div>
+            </div>
+
+            <!-- Produto -->
+            <div class="col-md-3">
+                <div class="card card-produto rounded-0 border-0 ">
+                <a href="produto.php"><img src="assets/images.2/produtos/thumb/camisas-etc.jpg" class="card-img-top rounded-0" alt="..."></a>
+                <div class="card-body">
+
+                    <h5 class="card-title">Camiseta manga comprida</h5>
+
+                    <p class="card-text">
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+
+                    </p>
+
+                    <p class="card-text">R$ 59,90</p>
+
+                </div>
+                </div>
+            </div>
+            <!-- /Produto -->
+
+            <!-- Produto -->
+            <div class="col-md-3">
+                <div class="card card-produto rounded-0 border-0">
+                <a href="produto.php"><img src="assets/images.2/produtos/thumb/camiseta-etc.jpg" class="card-img-top rounded-0" alt="..."></a>
+                <div class="card-body">
+
+                    <h5 class="card-title">Roupas Laranjas </h5>
+
+                    <p class="card-text">
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    </p>
+
+                    <p class="card-text">R$ 39,90</p>
+
+                </div>
+                </div>
+            </div>
+            <!-- /Produto -->
+
+            <!-- Produto -->
+            <div class="col-md-3">
+                <div class="card card-produto rounded-0 border-0">
+                <a href="produto.php"><img src="assets/images.2/produtos/thumb/chapeu-etc.jpg" class="card-img-top rounded-0" alt="..."></a>
+                <div class="card-body">
+
+                    <h5 class="card-title">Chápeus e Acessórios</h5>
+
+                    <p class="card-text">
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    </p>
+
+                    <p class="card-text">R$ 79,90</p>
+
+                </div>
+                </div>
+            </div>
+            <!-- /Produto -->
+
+            </div>
+
+        </div>
+        </main>
+        <!-- /.produtos -->
+
+
+
+
+
+        </div>
+        <!--- container --->
+
+    </main>
+
+
+<?php require __DIR__ . "/footer.php"; ?>
